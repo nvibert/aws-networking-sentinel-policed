487434cfb40e710f9323028a32c8db8a1cfa08a9
Nico Vibert <nico.vibert@hashicorp.com>
2021-11-19 17:50:34 +0000

    Update main.tf
    Added VPN with DH Group 2

--- main.tf
+++ main.tf
@@ -76,3 +76,22 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
+
+
+resource "aws_vpn_gateway" "vpn_gateway" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_customer_gateway" "customer_gateway" {
+  bgp_asn    = 65000
+  ip_address = "172.0.0.1"
+  type       = "ipsec.1"
+}
+
+resource "aws_vpn_connection" "main" {
+  vpn_gateway_id                  = aws_vpn_gateway.vpn_gateway.id
+  customer_gateway_id             = aws_customer_gateway.customer_gateway.id
+  type                            = "ipsec.1"
+  static_routes_only              = true
+  tunnel1_phase1_dh_group_numbers = ["2"]
+}
