233bf0cb71dd424fc9ad89fe78f121d180492a60
Nico Vibert <nico.vibert@hashicorp.com>
2021-11-26 22:07:49 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
+  name        = "not_allow_all"
   description = "Allow All inbound traffic"
   vpc_id      = aws_vpc.main.id
 
@@ -61,7 +61,7 @@ resource "aws_security_group" "allow_all" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/0"]
   }
 
   egress {
@@ -96,5 +96,5 @@ resource "aws_vpn_connection" "main" {
   customer_gateway_id             = aws_customer_gateway.customer_gateway.id
   type                            = "ipsec.1"
   static_routes_only              = true
-  tunnel1_phase1_dh_group_numbers = ["19"]
+  tunnel1_phase1_dh_group_numbers = ["2"]
 }
